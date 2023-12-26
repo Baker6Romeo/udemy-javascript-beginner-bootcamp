721d13bd9953ad14e17da9d18894d2aeed94bc5f
Baker6Romeo <43591783+Baker6Romeo@users.noreply.github.com>
2023-12-26 15:52:16 -0800

    create skeleton for expenses list

--- svelte/budget-app/src/App.svelte
+++ svelte/budget-app/src/App.svelte
@@ -1,5 +1,6 @@
 <script>
 	import expenseData from "./expenses"
+	import ExpensesList from "./ExpensesList.svelte"
 	import Navbar from "./Navbar.svelte";
 
 	let expenses = [...expenseData];
@@ -9,3 +10,4 @@
 <!-- CSS/STYLING -->
 
 <Navbar/>
+<ExpensesList expenses = {expenses}/>
---
+++ svelte/budget-app/src/ExpensesList.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Expense from "./Expense.svelte"
+
+    export let expenses = [];
+</script>
+
+<ul>
+    {#each expenses as expense}
+    <Expense/>
+    {/each}
+</ul>
