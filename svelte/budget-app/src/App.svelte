d2ea36dcd1ec965000bb637cb6c9f463d62f318a
Baker6Romeo <43591783+Baker6Romeo@users.noreply.github.com>
2023-12-26 14:18:00 -0800

    navbar component initial setup

--- svelte/budget-app/src/App.svelte
+++ svelte/budget-app/src/App.svelte
@@ -1,31 +1,16 @@
-<script>
-	export let name;
-</script>
+<!-- <script></script> -->
+<!-- JAVASCRIPT/LOGIC -->
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<i class="fas fa-home fa-5x"></i>
-</main>
+<!-- <style></style> -->
+<!-- CSS/STYLING -->
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<!-- HTML -->
+<nav class="nav">
+	<div class="nav-center">
+		<h1 class="nav-title">budget calculator</h1>
+		<button type="button" class="nav-btn">
+			<i class="far fa-plus-square" />
+				add item
+		</button>
+	</div>
+</nav>
