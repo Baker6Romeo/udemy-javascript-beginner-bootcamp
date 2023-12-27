6452c85aa0e275ac4680f6de62db8eb1d09d803d
Baker6Romeo <43591783+Baker6Romeo@users.noreply.github.com>
2023-12-27 10:50:33 -0800

    Amount toggle button

--- svelte/budget-app/src/Expense.svelte
+++ svelte/budget-app/src/Expense.svelte
@@ -2,16 +2,23 @@
     export let id;
     export let name = '';
     export let amount = 0;
+    let displayAmount = false;
+
+    function toggleAmount() {
+        displayAmount = !displayAmount;
+    }
 </script>
 
 <article class="single-expense">
     <div class="expense-info">
         <h2>{name}
-            <button class="amount-btn">
+            <button class="amount-btn" on:click={toggleAmount}>
                 <i class="fas fa-caret-down"/>
             </button>
         </h2>
+        {#if displayAmount}
         <h4>amount: ${amount}</h4>
+        {/if}
     </div>
     <div class="expense-buttons">
         <button class="expense-btn edit-btn">
